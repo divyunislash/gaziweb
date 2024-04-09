7ac817b4b2c9a7877adcdc077c56f0dbf8ecc355
성윤희 <s_yunhee96@naver.com>
2024-04-09 21:35:16 +0900

    test axios cors

--- front/src/config/axiosInstance.js
+++ front/src/config/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_HOST,
   headers: {
+    "Access-Origin": process.env.REACT_APP_HOST,
     "Access-Control-Allow-Credentials": true,
   },
 });
