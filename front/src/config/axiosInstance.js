d02e65060986c8ea5f84785117368f35421c1b6e
성윤희 <s_yunhee96@naver.com>
2024-04-11 23:23:46 +0900

    add option

--- front/src/config/axiosInstance.js
+++ front/src/config/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_URL,
   headers: {
-    "Access-Control-Allow-Origin": process.env.REACT_APP_URL,
+    "Access-Control-Allow-Origin": process.env.REACT_APP_HOST,
     "Access-Control-Allow-Credentials": true,
   },
   withCredentials: true,
