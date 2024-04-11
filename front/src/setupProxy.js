80434b9a7e23dc644726d80d701776350f1cd518
성윤희 <s_yunhee96@naver.com>
2024-04-11 22:35:21 +0900

    test4

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api", //proxy가 필요한 path parameter
     createProxyMiddleware({
-      target: process.env.REACT_APP_HOST, //타겟이 되는 api url
+      target: process.env.BASE_URL, //타겟이 되는 api url
       changeOrigin: true, // 서버 구성에 따른 호스트 헤더 변경 여부 설정
       pathRewrite: {
         "^/api": "", // URL ^/api -> 공백 변경
